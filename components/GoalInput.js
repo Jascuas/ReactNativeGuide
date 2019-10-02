47708e9e63b3a3650039023d65d8bda765d73bd3
Jascuas <javiersanchezdelago@hotmail.com>
2019-10-02 15:26:28 +0200

    Firts commit

---
+++ components/GoalInput.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
+const GoalInput = props => {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const goalInputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+  return (
+    <Modal visible={props.visible} animationType="slide">
+    <View style={styles.inputContainer}>
+      <TextInput
+        placeholder="Enter a goal"
+        style={styles.input}
+        onChangeText={goalInputHandler}
+        value={enteredGoal}
+      />
+      <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
+    </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: "center",
+        alignItems: "center"
+      },
+      input: {
+        width: "80%",
+        borderBottomColor: "black",
+        borderWidth: 1,
+        padding: 10
+      }
+})
+export default GoalInput;
